fefaf15a712bc614e7df27d68e0834455e29ece4
Robert Novotny <novotnr0@gmail.com>
2021-04-18 01:16:06 +0200

    Pridany fragment, layout a aktivita pre nastavenia appky

---
+++ app/src/main/java/com/github/novotnyr/android/gros/SettingsActivity.kt
@@ -0,0 +1,18 @@
+package com.github.novotnyr.android.gros
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+}
