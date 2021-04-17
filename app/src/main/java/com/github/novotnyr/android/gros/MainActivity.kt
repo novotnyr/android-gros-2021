60632c4dd73ea4e1bd9c5db694077abd4cabf33e
Robert Novotny <novotnr0@gmail.com>
2021-04-17 20:19:08 +0200

    Pridane tlacidlo pre platbu

--- app/src/main/java/com/github/novotnyr/android/gros/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/android/gros/MainActivity.kt
@@ -2,10 +2,15 @@ package com.github.novotnyr.android.gros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var payButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        payButton = findViewById(R.id.payButton)
     }
 }
