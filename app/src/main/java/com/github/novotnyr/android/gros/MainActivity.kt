83c9055c3bd2e28ffb19d1462f1be9ec44b65db4
Robert Novotny <novotnr0@gmail.com>
2021-04-17 20:59:15 +0200

    Pridany zakladny mechanizmus pre ziadost o povolenie

--- app/src/main/java/com/github/novotnyr/android/gros/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/android/gros/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.github.novotnyr.android.gros
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 
 class MainActivity : AppCompatActivity() {
     private lateinit var payButton: Button
@@ -15,5 +19,14 @@ class MainActivity : AppCompatActivity() {
         payButton = findViewById(R.id.payButton)
     }
 
-    fun onPayButtonClick(view: View) {}
+    fun onPayButtonClick(view: View) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+            == PermissionChecker.PERMISSION_GRANTED
+        ) {
+            Log.i("Gros", "Permission granted. Sending SMS")
+            TODO("Send SMS")
+        } else {
+            TODO("Permission not granted. Will not send SMS")
+        }
+    }
 }
